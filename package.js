0dd83721a4e23f344f53c1dacd0e19ad801e5ecc
Daria Voytova <d.voytova@2glab.com>
2016-11-07 15:11:40 +0300

    Change the library name; Point dependencies for package; Lint

--- package.js
+++ package.js
@@ -1,33 +1,43 @@
 Package.describe({
 	summary: "A roles based account management system using bootstrap 3",
-  version: "0.2.8",
+    version: "0.2.8",
 	name: "brylie:accounts-admin-ui",
-  git: "https://github.com/brylie/meteor-accounts-admin-ui-bootstrap-3.git"
+    git: "https://github.com/brylie/meteor-accounts-admin-ui-bootstrap-3.git"
 });
 
 Package.on_use(function (api, where) {
   api.versionsFrom("1.0.1");
-	api.use('standard-app-packages', ['client', 'server']);
-	api.use("alanning:roles@1.2.11", ['client', 'server']);
-	api.use("brylie:first-admin@0.1.0", ['server']);
+	api.use(["standard-app-packages",
+        "alanning:roles@1.2.11"
+        ], ["client", "server"]);
 
-	api.add_files('libs/user_query.js', ['client', 'server']);
+    api.use("brylie:first-user-admin@0.1.9", ["server"]);
 
-	api.add_files('client/startup.js', 'client');
-	api.add_files('client/accounts_admin.html', 'client');
-	api.add_files('client/accounts_admin.js', 'client');
-	api.add_files('client/delete_account_modal.html', 'client');
-	api.add_files('client/delete_account_modal.js', 'client');
-	api.add_files('client/info_account_modal.html', 'client');
-	api.add_files('client/info_account_modal.js', 'client');
-	api.add_files('client/update_account_modal.html', 'client');
-	api.add_files('client/update_account_modal.js', 'client');
-	api.add_files('client/update_roles_modal.html', 'client');
-	api.add_files('client/update_roles_modal.js', 'client');
+    api.use(["accounts-ui@1.0.0",
+        "accounts-password@1.3.0",
+        "kurounin:pagination-blaze@1.0.2",
+        "twbs:bootstrap@3.3.6"
+    ]);
 
-	api.add_files('style/style.css', 'client');
+	api.add_files("libs/user_query.js", ["client", "server"]);
 
-	api.add_files('server/startup.js', 'server');
-	api.add_files('server/publish.js', 'server');
-	api.add_files('server/methods.js', 'server');
+    api.add_files(["client/startup.js",
+        "client/accounts_admin.html",
+        "client/accounts_admin.js",
+        "client/delete_account_modal.html",
+        "client/delete_account_modal.js",
+        "client/info_account_modal.html",
+        "client/info_account_modal.js",
+        "client/update_account_modal.html",
+        "client/update_account_modal.js",
+        "client/update_roles_modal.html",
+        "client/update_roles_modal.js"
+    ], "client");
+
+	api.add_files("style/style.css", "client");
+
+	api.add_files(["server/startup.js",
+        "server/publish.js",
+        "server/methods.js"
+        ], "server");
 });
